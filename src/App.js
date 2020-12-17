6fd0031a970e728cda9b435d32fe3ea50e12838e
Raju KBN <raju.kbn04@gmail.com>
2020-12-17 23:47:38 +0530

    new line

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Application
+
         </a>
       </header>
     </div>
